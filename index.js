f6f5510c6bcc47d5e176b170050c66f91b7526f5
Charlie-Pencal <cpencal@gmail.com>
2023-08-28 22:08:55 -0300

    feat: criada a função que pegará e organiza

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// importação da biblioteca
 import rl from "readline-sync";
+
+// criação da array que vai conter as propriedades escritas no input
+const propriedades = [];
+
+// função com uma variavel let que utiliza question para gerar uma pergunta, onde a resposta vai ser convertida para letras maiusculas com o .toUpperCase e será armazenada.
+function pegarPropriedades() {
+    let input = rl.question("Digite uma propriedade (ou 'sair' para sair):").toUpperCase()
+
+    // o While como laço de repetição, para que só quando for escrito a palavra sair ele pare de repetir. Enquanto a palavra for diferente de sair, ele continuará repetindo, enquanto isso ele vai adicionar as palavras digitadas e somará a const propriedades.
+    while (input !== "SAIR") {
+        propriedades.push(input)
+        input = rl.question("Digite uma propriedade (ou 'sair' para sair):").toUpperCase()
+    }
+    // A const propriedades será organizada em ordem alfabetica pelo .sort. Utilizando o forEach, ele mostrará usando o console.log cada elemento do array.
+    propriedades.sort().forEach(elemento => {
+        console.log(elemento)
+    });
+
+}
+pegarPropriedades()
